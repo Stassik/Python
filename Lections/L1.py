1d3b477ee173a5f02a7f7dcac1a10bacbe90c5bd
Anastasiya Evteeva <nastase4ka@mail.ru>
2023-01-25 23:51:49 +0300

    modified L1

--- Lections/L1.py
+++ Lections/L1.py
@@ -1,7 +1,16 @@
-a = 8
+"""a = 8
 b = 6.23
 c = 'строка'
 
 print(a, b, c)
 print(f"{a} - {b} - {c}")
-print("{} | {} | {}".format(a, b, c))
+print("{} | {} | {}".format(a, b, c))"""
+
+
+n = 4423
+sum = 0
+while n > 0:
+    m = n % 10
+    sum += m
+    n //= 10
+print(sum)
