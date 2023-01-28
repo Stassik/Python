7603b2a913d8473b301af5bee1fb39969a4d73f9
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-01-29 00:22:03 +0300

    modified file Task7

--- Seminars/Task7.py
+++ Seminars/Task7.py
@@ -10,3 +10,10 @@
 Input: 2016
 Output: YES
 """
+
+year = int(input("Введите год: "))
+
+if year % 4 == 0 and year % 100 != 0 or year % 400 == 0:
+    print("Год Высокосный")
+else:
+    print("Обычный год")
