8eb9857dde14b2a970fb3278bb8454da8f8458be
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-01-29 00:43:15 +0300

    modified file Task2

--- Seminars/Task2.py
+++ Seminars/Task2.py
@@ -5,3 +5,19 @@
 123 -> 6 (1 + 2 + 3)
 100 -> 1 (1 + 0 + 0)
 """
+number = int(input("Введите трехзначное число: "))
+
+
+def calc_sum(num):
+    sum = 0
+    while num != 0:
+        sum += num % 10
+        num = num//10
+    return sum
+
+
+if number > 999 or number < 100:
+    print("Вы ввели не трехзначное число.")
+else:
+    total = calc_sum(number)
+    print(total)
