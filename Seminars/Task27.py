feb000ebd6a38b9cba7aa3269196667f8afdd7ea
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-02-10 22:13:23 +0300

    modified file Task27

--- Seminars/Task27.py
+++ Seminars/Task27.py
@@ -10,6 +10,5 @@ Output: 13
 text = "She sells sea shells on the sea shore The shells that she sells are sea shells I'm sure.So if she sells sea \
 shells on the sea shore I'm sure that the shells are sea shore shells"
 
-words = text.lower().split()
-s = set(words)
-print(len(s))
+words = set(text.lower().split())
+print(len(words))
