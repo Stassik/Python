3e57a1777eb081753f1a44034b9147a432c5a686
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-02-10 21:54:21 +0300

    modified file Task25

--- Seminars/Task25.py
+++ Seminars/Task25.py
@@ -8,4 +8,18 @@ Output: a a_1 a_2 b c a_3 a_4 d c_1 d_1 d_2
 """
 word = "a a a b c a a d c d d"
 my_list = word.split()
+result = ''
 print(my_list)
+
+d = dict()
+
+for i in range(len(my_list)):
+    if my_list[i] not in d:
+        d[my_list[i]] = 1
+        result += f'{my_list[i]} '
+    else:
+        result += f'{my_list[i]}_{d[my_list[i]]} '
+        d[my_list[i]] += 1
+
+print(result)
+print(d)
