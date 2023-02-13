1cb9ed9c5a36b3d378a21eac49807cd0a51d7228
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-02-13 10:23:13 +0300

    modified file Task28

--- Seminars/Task28.py
+++ Seminars/Task28.py
@@ -4,3 +4,26 @@
 Также нельзя использовать циклы
 """
 
+def rec_sum(x, y, total=0):
+    if x>=1 or y>=1:
+        total +=1
+        if x>y:
+            x -=1
+        else:
+            y -= 1
+        print(total)
+        return rec_sum(x, y, total)
+    return total            
+
+a = int(input("Введите число А: "))
+b = int(input("ВВедите число В: "))
+
+while a <0 or b<0:
+    print("Вы ввели отрицательное число.")
+    if a<0:
+        a = int(input("Введите число А: "))
+    else:
+        b = int(input("ВВедите число В: "))
+
+total = rec_sum(a, b)
+print(total)
