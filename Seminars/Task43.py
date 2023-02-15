7c63a98a71e2e5411dbe75470f0ac99163ec101a
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-02-15 23:58:32 +0300

    add new file Task43

---
+++ Seminars/Task43.py
@@ -0,0 +1,26 @@
+"""
+Дан список чисел. Посчитайте, сколько в нем пар элементов, равных друг другу. 
+Считается, что любые два элемента, равные друг другу образуют одну пару,
+которую необходимо посчитать. Вводится список чисел. 
+Все числа списка находятся на разных строках.
+"""
+
+def fill_list(x):
+    x_list = list()
+    for i in range(x):
+        x_list.append(int(input(f"Введите {i+1} число: ")))
+    return x_list
+
+def count_num(x_list):
+    count = 0
+    for i in range(len(x_list)-1):
+        num = x_list.pop(0)   
+        if num not in x_list:
+            count += 1
+    return count
+
+n = int(input("Введите длину массива: "))
+n_list = fill_list(n)
+total = count_num(n_list)
+print(n_list)
+print(total)
