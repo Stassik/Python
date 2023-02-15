329154379e51b66c45ae639f0e61359f197d8a52
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-02-15 23:48:57 +0300

    modifird file Task41

--- Seminars/Task41.py
+++ Seminars/Task41.py
@@ -7,3 +7,21 @@
 Далее записаны N чисел — элементы массива. 
 Массив состоит из целых чисел.
 """
+def fill_list(x):
+    x_list = list()
+    for i in range(x):
+        x_list.append(int(input(f"Введите {i+1} число: ")))
+    return x_list
+
+def count_num(x_list):
+    count = 0
+    for i in range(1, len(x_list)-1):   
+        if x_list[i]>x_list[i+1] and x_list[i]>x_list[i-1]:
+            count += 1
+    return count        
+
+n = int(input("Введите длину массива: "))
+n_list = fill_list(n)
+total = count_num(n_list)
+print(n_list)
+print(total)
