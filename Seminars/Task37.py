baf4eb12ad167fe88512ead05bcc3795f6937117
Anastasiyac:/Users/HP/Desktop/Обучение/Разработчик/Python/.folder/Scripts/python.exe <nastase4ka@mail.ru>
2023-02-26 01:07:43 +0300

    modified Task37

--- Seminars/Task37.py
+++ Seminars/Task37.py
@@ -9,6 +9,38 @@
 (Например имя или фамилию человека)
 4. Использование функций. Ваша программа
 не должна быть линейной
-
 """
+
+#Импорт данных в справочник
+
+def export_to_phonebook(new_line):
+    with open('phonebook.txt', 'a', encoding='utf-8') as data:
+        data.writelines(new_line)
+
+def import_to_phonebook():
+    with open('phonebook.txt', 'r', encoding='utf-8') as data:
+        contact = data.readline()
+        print(contact)
+        
+def input_contact():
+    new_contact = dict()
+    new_contact['Фамилия'] = ''
+    new_contact['Имя'] = ''
+    new_contact['Отчество'] = ''
+    new_contact['Телефон'] = ''
+
+    for item in new_contact:
+        new_contact[item] = input(f'{item}: ')
+        export_to_phonebook(" {}: {} |".format(item, new_contact[item]))
+
+
+#input_contact()
+import_to_phonebook()
+
+
+    
+
+
+
+        
 
